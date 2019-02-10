6d989dff3a9c3c4fef8a08c6b4fe2afa084c934a
Ahmad Raza <raza2157@gmail.com>
2019-02-10 16:08:05 -0500

    Heroku file-name bug fix

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import "./styles/primitive.css";
+import "./styles/Primitive.css";
 import "./styles/main.css";
 import axios from "axios";
 import Form from "./Form";
